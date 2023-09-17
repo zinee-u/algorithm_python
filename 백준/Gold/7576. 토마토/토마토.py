d0fecc8778f4a50144285687a860cf592be7e4e3
Sujin Yu <86346615+zinee-u@users.noreply.github.com>
2023-09-17 19:12:18 +0900

    [Gold V] Title: 토마토, Time: 2900 ms, Memory: 122556 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,32 +1,50 @@
-import sys
 from collections import deque
+m, n = map(int, input().split())
+# print(n, m)
+# 정수 1은 익은 토마토, 정수 0은 익지 않은 토마토, 정수 -1은 토마토가 들어있지 않은 칸
 
-n, m = map(int, sys.stdin.readline().split())
+# arr = [list(map(int, input().split())) for _ in range(n)]
+arr = []
+# print(arr)
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+    # print(list(map(int, input().split())))
+
+# print(arr)
+
 dr, dc = [-1, 0, 1, 0], [0, -1, 0, 1]
 q = deque()
-arr = []
-for i in range(m):
-    arr.append(list(map(int, sys.stdin.readline().split())))
-    for j in range(n):
-        if arr[i][j] == 1: q.append([i, j])
+visited = [[0 for _ in range(m)] for _ in range(n)]
+sum_day = 0
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 1:
+            q.append([i, j, 0])
+            visited[i][j] = 1
+        elif arr[i][j] == -1:
+            visited[i][j] == -1
+            sum_day += 1
+
+max_day = 0
 
 while q:
-    r, c = q.popleft()
+    r, c, day = q.popleft()
     for d in range(4):
         nr, nc = r + dr[d], c + dc[d]
-        if -1 < nr < m and -1 < nc < n and arr[nr][nc] == 0:
-            arr[nr][nc] = arr[r][c] + 1
-            q.append([nr, nc])
-            
-check, max_days = True, 0
+        if -1 < nr < n and -1 < nc < m:
+            if arr[nr][nc] == 0 and visited[nr][nc] == 0:
+                visited[nr][nc] = day + 1
+                q.append([nr, nc, day + 1])
+    max_day = max(max_day, day)
+# print(visited)
 
-for i in arr:
-    max_days = max(max(i), max_days)
-
-for i in arr:
-    if 0 in i:
-        check = False
-        print(-1)
-        break
-
-if check: print(max_days-1)
+for i in range(n):
+    for j in range(m):
+        if visited[i][j] == 0:
+            sum_day -= 1
+if sum_day != 0:
+    answer = -1
+else:
+    # answer = max(map(max, visited))
+    answer = max_day
+print(answer)
