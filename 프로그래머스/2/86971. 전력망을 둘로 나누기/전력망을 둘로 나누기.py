87cf4ead39a8a82762bb5916f08b071ca4a090b7
Sujin Yu <86346615+zinee-u@users.noreply.github.com>
2024-03-31 21:00:20 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.66 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,36 +1,34 @@
 from collections import deque
-
 def solution(n, wires):
-	answer = float("INF")
-	graph = [[] for _ in range(n + 1)]
-	q = deque()
-	
-	for w in wires:			
-		n1, n2 = map(int, w)
-		graph[n1].append(n2)
-		graph[n2].append(n1)
-
-	for i in range(n-1):
-		node = wires[i][1]
-		q.append([i, node])
-		answer = min(bfs(q, graph, n, wires), answer)
-	return answer
-
-
-def bfs(q, graph, n, wires):
-	area = 1
-	visited = [0 for _ in range(n + 1)]
-	visited[q[0][1]] = 1
-	
-	while q:
-		e, n1 = q.popleft()
-		for n2 in graph[n1]:
-			if (wires[e][0] == n1 and wires[e][1] == n2) or (wires[e][0] == n2 and wires[e][1] == n1):
-				continue
-			if visited[n2] == 1: continue
-			area += 1
-			q.append([e, n2])
-			visited[n2] = 1
-
-	diff = abs(n-2*area)
-	return diff
+    answer = n
+    graph = [[] for _ in range(n)]
+    for start, end in wires:
+        graph[start - 1].append(end - 1)
+        graph[end - 1].append(start - 1)
+    
+    for start in range(n):
+        for end in graph[start]:
+            if not end: continue
+            cut = [[0 for _ in range(n)] for _ in range(n)]
+            cut[start][end] = 1
+           
+            visited = [0 for _ in range(n)]
+            q = deque([start])
+            visited[start] = 1
+            
+            # 기준 node 영역을 선 방문처리 했으므로 1부터 시작
+            cnt = 1
+            while q:
+                n_cur = q.popleft()
+                for n_nxt in graph[n_cur]:
+                    visited[n_cur] = 1
+                    if not visited[n_nxt] and not cut[n_cur][n_nxt]:
+                        visited[n_nxt] = 1
+                        q.append(n_nxt)
+                        cnt += 1
+            # 기준 node에서 개수 세기 끝난 시점
+            area1 = n - cnt
+            area2 = n - area1
+            # |area1 - area2|가 가장 작은 값 반환
+            answer = min(abs(area1 - area2), answer)
+    return answer
