cd1dbc115e9284496dc568b90c05b4a0e99a94c5
Sujin Yu <86346615+zinee-u@users.noreply.github.com>
2024-06-02 11:19:32 +0900

    [Bronze I] Title: 색종이, Time: 1768 ms, Memory: 39120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,40 +1,15 @@
-from collections import deque
 n = int(input())
+area = [[0 for _ in range(1001)] for _ in range(1001)]
 pos = []
-visited = [[0 for _ in range(1002)] for _ in range(1002)]
 
-
-
-def bfs(q):
-	cnt = 1
-	ic, ir, w, h = q[0][0], q[0][1], q[0][2], q[0][3]
-	visited[ir][ic] = 1
-	# print("ic,",ic, ir, w, h)
-	while q:
-		c, r, w, h = q.popleft()
-		for i in range(h):
-			for j in range(w):
-				nr, nc = r+i, c+j
-				# print(nr, nc)
-				if -1 < nr < ir+h and -1 < nc < ic+w:
-					if not visited[nr][nc]:
-						visited[nr][nc] = 1
-						cnt += 1
-						q.append([nc, nr, w, h])
-			# print(q)
-	return cnt
-
-area = []
-for _ in range(n):
+for i in range(n):
 	pos.append(list(map(int, input().split())))
-	# print("pos=",pos)
+	ic, ir, w, h = pos[i]
+	for j in range(ir, ir+h):
+		area[j][ic:ic+w] = [i+1] * w
 
-for i in range(n-1, -1, -1):
-	# print(pos[i][1], pos[i][0])
-	q = deque()
-	q.append(pos[i])
-	area.append(bfs(q))
-	# print(area)
-
-for i in range(n-1, -1, -1):
-	print(area[i])
+for i in range(1, n+1):
+	answer = 0
+	for r in range(1001):
+		answer += area[r].count(i)
+	print(answer)
