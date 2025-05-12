ffa54d922c86d69ebc74b7903e6d1a9538d75498
Sujin Yu <86346615+zinee-u@users.noreply.github.com>
2025-05-12 22:59:57 +0900

    [Silver III] Title: 소수 구하기, Time: 336 ms, Memory: 40660 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,13 @@
+M, N = map(int, input().split())
+
+is_prime = [True for _ in range(N+1)]
+is_prime[0], is_prime[1] = False, False
+
+for i in range(2, int(N**0.5)+1):
+    if is_prime[i]:
+        for j in range(i*i, N+1, i):
+            is_prime[j] = False
+
+for i in range(M, N+1):
+    if is_prime[i]:
+        print(i)
