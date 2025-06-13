61b333ec97152e955439a60e0ea9d382cd26d765
Sujin Yu <86346615+zinee-u@users.noreply.github.com>
2025-06-14 01:22:12 +0900

    [Silver I] Title: 안전 영역, Time: 780 ms, Memory: 34992 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+
+N = int(input())
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+# print(arr)
+
+max_val = max(map(max, arr))
+# print(max_val)
+# Sujin LOVE
+# LOVE SUJIN
+
+dr, dc = [0, -1, 0, 1], [-1, 0, 1, 0]
+max_cnt = -1
+
+def bfs(q):
+  while q:
+    r, c, cnt = q.popleft()
+    for d in range(4):
+      nr, nc = r + dr[d], c + dc[d]
+      if(-1 < nr < N and -1 < nc < N and visited[nr][nc] == 0):
+        if arr[nr][nc] > h:
+          visited[nr][nc] = cnt
+          q.append([nr, nc, cnt])
+
+for h in range(max_val):
+  q = deque()
+  cnt = 1
+  visited = [[0 for _ in range(N)] for _ in range(N)]
+  for i in range(N):
+    for j in range(N):
+      if arr[i][j] > h and visited[i][j] == 0:
+        visited[i][j] = cnt
+        q.append([i, j, cnt])
+        bfs(q)
+        cnt += 1
+        # print(cnt)
+      # print(visited)
+  max_cnt = max(max_cnt, max(map(max, visited)))
+print(max_cnt)
