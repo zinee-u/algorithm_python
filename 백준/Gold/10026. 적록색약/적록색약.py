672da19ffedde51c66ae8295b110b4eee6caf211
Sujin Yu <86346615+zinee-u@users.noreply.github.com>
2025-06-14 23:02:28 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,74 +1,35 @@
-import sys
-import copy
 from collections import deque
+
 N = int(input())
-arr = [list(map(str, sys.stdin.readline().rstrip())) for _ in range(N)]
-arr2 = copy.deepcopy(arr)
-for i in range(N):
-  for j in range(N):
-    if arr2[i][j] == 'R':
-      arr2[i][j] = 'G'
-# print(arr2)
+arr_rgb = [list(input().strip()) for _ in range(N)]
 # print(arr)
-# 색약 빨초2 파1 정상 빨3 파2 초1
+arr_gb = [['G' if e == 'R' else e for e in row] for row in arr_rgb]
+# print(arr_rgcb)
 
 dr, dc = [0, -1, 0, 1], [-1, 0, 1, 0]
-q1 = deque()
-q2 = deque()
-visited1 = [[0 for _ in range(N)] for _ in range(N)]
-visited2 = [[0 for _ in range(N)] for _ in range(N)]
-area_able, area_disable = 0, 0
 
-def bfs(q):
+def bfs(q, visited, arr):
+  visited[q[0][0]][q[0][1]] = 1
+  col = arr[q[0][0]][q[0][1]]
   while q:
-    r, c, cnt, col, eye = q.popleft()
+    r, c = q.popleft()
     for d in range(4):
       nr, nc = r + dr[d], c + dc[d]
-      if -1 < nr < N and -1 < nc < N:
-        # able
-        if eye == 1:
-          if visited1[nr][nc] == 0:
-            if arr[nr][nc] == col:
-              visited1[nr][nc] = cnt
-              q.append([nr, nc, cnt, col, 1])
-        #disable
-        if eye == 2:
-          if visited2[nr][nc] == 0:
-            if arr2[nr][nc] == col:
-              visited2[nr][nc] = cnt
-              q.append([nr, nc, cnt, col, 2])
-            
-cnt, cnt2 = 1, 1
-for i in range(N):
-  for j in range(N):
-    if visited1[i][j] == 0:
-      if arr[i][j] == 'R':
-        visited1[i][j] = cnt
-        q1.append([i, j, cnt, 'R', 1])
-        bfs(q1)
-      elif arr[i][j] == 'G':
-        visited1[i][j] = cnt
-        q1.append([i, j, cnt, 'G', 1])
-        bfs(q1)
-      else:
-        visited1[i][j] = cnt
-        q1.append([i, j, cnt, 'B', 1])
-        bfs(q1)
-      cnt += 1
-      # print(visited1)
-    if visited2[i][j] == 0:
-      if arr2[i][j] == 'B':
-        visited2[i][j] = cnt2
-        q2.append([i, j, cnt2, 'B', 2])
-        bfs(q2)
-      else:
-        visited2[i][j] = cnt2
-        q2.append([i, j, cnt2, 'G', 2])
-        bfs(q2)
-      cnt2+=1
-    # print(visited2)
+      if(-1 < nr < N and -1 < nc < N):
+        if not visited[nr][nc] and arr[nr][nc] == col:
+          visited[nr][nc] = 1
+          q.append([nr, nc])
 
-area_able = max(map(max, visited1))
-area_disable = max(map(max, visited2))
+def cnt_area(arr):
+  visited = [[0 for _ in range(N)] for _ in range(N)]
+  area = 0
+  q = deque()
+  for i in range(N):
+    for j in range(N):
+      if not visited[i][j]:
+        q.append([i, j])
+        bfs(q, visited, arr)
+        area += 1
+  return area
 
-print(area_able, area_disable)
+print(cnt_area(arr_rgb), cnt_area(arr_gb))
