57727f7c24cf8e695bd5cbdb0692d425171bd212
Sujin Yu <86346615+zinee-u@users.noreply.github.com>
2025-06-23 22:21:01 +0900

    [Gold IV] Title: 연구소, Time: 2316 ms, Memory: 35044 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,68 +1,73 @@
 from collections import deque
 import copy
 
-n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
-
-visited_w = [[0 for _ in range(m)] for _ in range(n)]
+N, M = map(int, input().split())
+# print(N, M)
+arr = [list(map(int, input().split())) for _ in range(N)]
 
 dr, dc = [0, -1, 0, 1], [-1, 0, 1, 0]
 
-# 0b 1w 2v
+
+def bfs(pos_virus, v):
+  q = deque()
+  for i, j in pos_virus:
+    q.append([i, j])
+  while q:
+    r, c = q.popleft()
+    for d in range(4):
+      nr, nc = r+dr[d], c+dc[d]
+      if -1 < nr < N and -1 < nc < M:
+        if v[nr][nc] == 0:
+          v[nr][nc] = 2
+          q.append([nr, nc])
+  return v
+
+
+#Choose 3-position
+
+def recur(idx, cnt):
+  global pos_virus, max_cnt, pos, cnt_empty, lst
+  
+  if cnt == 3:
+    v = copy.deepcopy(arr)
+    for i, j in lst:
+      v[i][j] = 1
+    res_arr = bfs(pos_virus, v)
+    cnt = 0
+    for i in range(N):
+      for j in range(M):
+        if res_arr[i][j] == 0:
+          cnt += 1
+    max_cnt = max(cnt, max_cnt)
+    return
+
+  if idx == cnt_empty:
+    return
+
+  lst.append(pos[idx])
+  recur(idx+1, cnt+1)
+  lst.pop()
+  recur(idx+1, cnt)
 
 q = deque()
-				
 
+pos = []
 lst = []
-max_area = 0
+pos_virus=[]
+max_cnt = -1
+cnt_empty = 0
+
+for i in range(N):
+  for j in range(M):
+    if arr[i][j] == 0:
+      pos.append([i, j])
+      cnt_empty += 1
+    if arr[i][j] == 2:
+      pos_virus.append([i,j])
+
+# print(q)
 
 
-def dfs(dep):
-	if dep == 3:
-		# print(visited_w)
-		
-		bfs(visited_w)
-		return
+recur(0, 0)
 
-	for i in range(n):
-		for j in range(m):
-			if arr[i][j] == 0 and not visited_w[i][j]:
-				visited_w[i][j] = 1
-				dfs(dep + 1)
-				visited_w[i][j] = 0
-
-
-def bfs(visited_w):
-	global max_area
-	cp_arr = copy.deepcopy(arr)
-	visited_v = [[0 for _ in range(m)] for _ in range(n)]
-	for i in range(n):
-		for j in range(m):
-			if visited_w[i][j] == 1:
-				cp_arr[i][j] = 1
-			if cp_arr[i][j] == 2 and not visited_v[i][j]:
-				q.append([i, j])
-				visited_v[i][j] = 1
-	
-	while q:
-		r, c = q.popleft()
-		for d in range(4):
-			nr, nc = r + dr[d], c + dc[d]
-			if -1 < nr < n and -1 < nc < m and cp_arr[nr][nc] == 0 and not visited_v[nr][nc]:
-				cp_arr[nr][nc] = 2
-				visited_v[nr][nc] = 1
-				q.append([nr, nc])
-
-	# print(cp_arr)
-	cnt = 0
-	for i in range(n):
-		for j in range(m):
-			if cp_arr[i][j] == 0:
-				cnt += 1
-				max_area = max(max_area, cnt)
-	
-# main
-
-dfs(0)
-
-print(max_area)
+print(max_cnt)
